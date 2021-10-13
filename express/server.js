904ec5cbea5cec18bdd809c7283afbdcd541ed15
Justin Drapeau <jmdrapea@uwaterloo.ca>
2021-10-13 19:15:01 -0400

    Create wallet or account for user

--- express/server.js
+++ express/server.js
@@ -3,12 +3,14 @@ require('dotenv').config()
 
 // Routes
 const profile = require("./routes/api/profile.js");
+const user = require("./routes/api/user.js");
 
 var app = express();
 
 const port = process.env.PORT || 5000
 
 app.use("/api/profile", profile);
+app.use("/api/user", user);
 
 app.listen(port, () => {
 	console.log(`Listening on http://localhost:${port}`)
