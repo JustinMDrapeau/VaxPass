eff7f5b662d804455551e9402f20bd040baddfd8
TN <32180978+tharseken@users.noreply.github.com>
2022-01-12 17:08:06 -0500

    User Creation (#6)
    * Created user creation route that creates user, wallet, and identification in DB and returns response.
    
    * Updating PR to address PR comments

--- fe/src/http-common.ts
+++ fe/src/http-common.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api", //TODO: change this to the real endpoint
+  baseURL: "http://localhost:5000/api", //TODO: change this to the real endpoint
   headers: {
     "Content-type": "application/json"
   }
