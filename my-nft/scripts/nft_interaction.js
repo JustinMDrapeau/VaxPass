35c567d881975c80d4d38df70c6543e66241ced1
sharan16 <sharan.somaskanthan@gmail.com>
2022-02-03 19:06:31 -0500

    added a new interaction for clinic login

--- my-nft/scripts/nft_interaction.js
+++ my-nft/scripts/nft_interaction.js
@@ -3,12 +3,12 @@ const API_URL = process.env.API_URL;
 const PUBLIC_KEY = process.env.PUBLIC_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const PATIENT_PUBLIC_KEY = process.env.PATIENT_PUBLIC_KEY;
-
+const Wallet = require('ethereumjs-wallet').default
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
 const web3 = createAlchemyWeb3(API_URL);
 
 const contract = require("../artifacts/contracts/vaxNFT.sol/VaxNFT.json");
-const contractAddress = "0x0B0A8DA24d0aFd826956E85e0944C18F37De6C9E";
+const contractAddress = "0xad5306305153930e6090FE430aF4573E356B6c3C";
 const nftContract = new web3.eth.Contract(contract.abi, contractAddress);
 
 async function mintNFT(firstName, lastName, manufacturer, phase) {
@@ -121,11 +121,30 @@ async function clinicSignup(pubk, privk, name, address, email) {
   });
 }
 
+async function clinicLogin(pubk, privk) {
+  const nonce = await web3.eth.getTransactionCount(pubk, 'latest'); // Get latest nonce
+
+  // The transaction that connects the clinic account to the smart contract
+  const tx = {
+    'from': pubk,
+    'to': contractAddress,
+    'nonce': nonce,
+    'gas': 500000,
+    'maxPriorityFeePerGas': 1999999987,
+    'data': nftContract.methods.clinicLogin().encodeABI()
+  };
+
+  const signPromise = web3.eth.accounts.signTransaction(tx, privk);
+  signPromise.then((signedTx) => {
+    web3.eth.sendSignedTransaction(signedTx.rawTransaction).on('receipt', console.log)
+  })
+}
+
 // mintNFT('Tharse', 'Yokan', 'Pfizer', 1)
 
 // clinicSignup(PUBLIC_KEY, PRIVATE_KEY, 'Hospital of Toronto', '25 Bay Street Toronto Canada L8B93W', 'admin@torontohospital.com')
 
-
+// clinicLogin(PUBLIC_KEY, PRIVATE_KEY)
 // nftContract.methods.walletIdToClinic(PUBLIC_KEY).call()
 // .then((result) => {
 //   console.log(result)
@@ -134,6 +153,9 @@ async function clinicSignup(pubk, privk, name, address, email) {
 //     console.log(err)
 // })
 
+// let sig = web3.eth.sign("test", PRIVATE_KEY);
+// console.log(sig)
+console.log(Wallet.fromPrivateKey(Buffer.from(PRIVATE_KEY, 'hex')).getAddress().toString('hex'));
 // Test that the map tokenIdTokenInfo (tokenId -> NFT metadata) contains the newly minted NFT
 
 // nftContract.methods.tokenIdTokenInfo(1).call()
