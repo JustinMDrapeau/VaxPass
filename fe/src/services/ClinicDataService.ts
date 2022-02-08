7aff84dee8c4a995cc736e585ee10480b196a4dc
Tharseken Navajeevayokan <tharseken.n@gmail.com>
2022-02-07 19:57:37 -0500

    Fixing the ID issue with the mint and transfer function

--- fe/src/services/ClinicDataService.ts
+++ fe/src/services/ClinicDataService.ts
@@ -1,20 +1,20 @@
 import ContractService from "./ContractService";
 import TransactionRequest from "../types/TransactionRequest";
 
+const TOPIC_ID= "0x65f4dc08c4c2661a2ccb6e433da77c60168189d45e70829ac6cac16dc09e4984";
+
 class ClinicDataService {
 
   async mintAndTransfer(clinicPublicKey: string, clinicPrivateKey: string, product: string, lot: string, phase: number, date: string, patientPublicKey: string) {
-
-    const ID_GOES_HERE = 4;
-
     let mintRequest: TransactionRequest = {}
     mintRequest.data = ContractService.getContract().methods.mintNFT(product, lot, phase, date).encodeABI()
     
     const mintingSignedTransaction = await ContractService.signTransaction(mintRequest, clinicPublicKey, clinicPrivateKey);
     const mintReceipt = await ContractService.sendSignedTransaction(mintingSignedTransaction.rawTransaction as string).on("receipt", (receipt : any)=>{});
+    const transactionLog = mintReceipt.logs.filter((log : any) => {return log.topics.includes(TOPIC_ID)})[0]    // TOPIC ID changes if the smart contract is recreated
 
     let transferRequest: TransactionRequest = {}
-    transferRequest.data = ContractService.getContract().methods.transferFrom(clinicPublicKey, patientPublicKey, ID_GOES_HERE).encodeABI()
+    transferRequest.data = ContractService.getContract().methods.transferFrom(clinicPublicKey, patientPublicKey, parseInt(transactionLog.data)).encodeABI()
 
     const transferSignedTransaction = await ContractService.signTransaction(transferRequest, clinicPublicKey, clinicPrivateKey);
     const transferReceipt = await ContractService.sendSignedTransaction(transferSignedTransaction.rawTransaction as string).on("receipt", (receipt : any) => {});
--- fe/src/services/ContractService.ts
+++ fe/src/services/ContractService.ts
@@ -2,7 +2,7 @@ import { createAlchemyWeb3 } from "@alch/alchemy-web3";
 import contractArtifact from "../VaxNFT.json";
 import TransactionRequest from "../types/TransactionRequest";
 
-const CONTRACT_ADDRESS = "0x438363746455ef25661f5b2F2D0FeF0F75324d2e"
+const CONTRACT_ADDRESS = "0x8d998FdD3505A1388D49230E41BEDaCDba8a6d13"
 const web3 = createAlchemyWeb3(process.env.REACT_APP_ALCHEMY_API_URL as string);
 const contract = new web3.eth.Contract(contractArtifact.abi as any, CONTRACT_ADDRESS);
 
