27da9f10cbc700af63ad71241612a2ad37c04448
Tharseken Navajeevayokan <tharseken.n@gmail.com>
2022-02-27 16:11:42 -0500

    Adding GitHub actions deployment file and removing Express backend code

--- express/config/keys_prod.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: process.env.PROD_MONGO_URI,
-    encryptionKey : process.env.PROD_ENCRYPTION_KEY
-};
--- express/utils/http-response-creator.js
+++
@@ -1,3 +0,0 @@
-module.exports = function httpResponse(res, status, json) {
-    return res.status(status).json(json);
-}
